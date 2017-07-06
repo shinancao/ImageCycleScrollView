72c6b04f22d175a1a20a19366df51b26d559c19f
shinancao <shinancao666@163.com>
2017-07-06 16:51:54 +0800

    config demo

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import ImageCycleScrollView
 
 class ViewController: UIViewController {
-
+    let cycleScrollView: ImageCycleScrollView = {
+        let `self` = ImageCycleScrollView(frame: CGRect(x: 0, y: 100, width: UIScreen.main.bounds.width, height: 177))
+        self.pageControlShowStyle = .center
+        self.pageControl?.currentPageIndicatorTintColor = UIColor.purple
+        return self
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(cycleScrollView)
+        
+        cycleScrollView.imageUrls = ["http://ojx1pmrk7.bkt.clouddn.com/ALetterToMe.jpg", "http://ojx1pmrk7.bkt.clouddn.com/Me-Pressent-Future.jpg", "http://ojx1pmrk7.bkt.clouddn.com/Not-Always-May.jpg", "http://ojx1pmrk7.bkt.clouddn.com/Reveal-App-Layout.jpg"]
     }
 
     override func didReceiveMemoryWarning() {
